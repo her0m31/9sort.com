cf4842e3b9e2987d38440f171fe7d3830e51ca11
her0m31 <eda.1380@gmail.com>
2015-07-05 21:44:15 +0900

    create qiita-php.php

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<?php
-$request = "https://www.kimonolabs.com/api/68jpwwa8?apikey=tuqSaJHUsKLOxKJBPr7vcxILzXfVDUIy";
+<?
+$request = "https://www.kimonolabs.com/api/7lyezar2?apikey=tuqSaJHUsKLOxKJBPr7vcxILzXfVDUIy";
 $response = file_get_contents($request);
 $results = json_decode($response, TRUE);
-
-for($i = 0; $i < 800; $i++) {
-  echo '<a href="';
-  echo $results['results']['collection2'][$i]['Title']['href'];
-  echo '">';
-  echo htmlspecialchars($results['results']['collection2'][$i]['Title']['text'], ENT_QUOTES, 'UTF-8');
-  echo "</a><br>";
-}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PURE</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+      <?
+        for($i = 0; $i < 12; $i++) {
+          echo "<img src='http://capture.heartrails.com/small/?";
+          echo $results['results']['php'][$i]['title']['href'] . "'/>";
+          echo $results['results']['php'][$i]['title']['text'];
+          echo "<br>";
+        }
+      ?>
+  </body>
+</html>
---
+++ qiita-php.php
@@ -0,0 +1,27 @@
+<?
+$request = "https://www.kimonolabs.com/api/7lyezar2?apikey=tuqSaJHUsKLOxKJBPr7vcxILzXfVDUIy";
+$response = file_get_contents($request);
+$results = json_decode($response, TRUE);
+
+//タイトル,URL,日付,タグ１,タグ2,タグ3,タグ4,コメント数,ストック数
+$fileName = "qiita-php.csv";
+if(is_writable($fileName)) {
+  $CountQiitaPhp = count($results['results']['php']);
+  for($i = 0; $i < $CountQiitaPhp; $i++) {
+    $title    = $results['results']['php'][$i]['title']['text'];
+    $title    = str_replace(",", "、", $title);
+
+    $url      = $results['results']['php'][$i]['title']['href'];
+    $date     = $results['results']['php'][$i]['date']['text'];
+    $tag1     = $results['results']['php'][$i]['tag1']['text'];
+
+    $tag2     = empty($results['results']['php'][$i]['tag2']['text'])     ? 0 : $results['results']['php'][$i]['tag2']['text'];
+    $tag3     = empty($results['results']['php'][$i]['tag3']['text'])     ? 0 : $results['results']['php'][$i]['tag3']['text'];
+    $tag4     = empty($results['results']['php'][$i]['tag4']['text'])     ? 0 : $results['results']['php'][$i]['tag4']['text'];
+    $comments = empty($results['results']['php'][$i]['comments']['text']) ? 0 : $results['results']['php'][$i]['comments']['text'];
+    $stock    = empty($results['results']['php'][$i]['stock'])            ? 0 : $results['results']['php'][$i]['stock'];
+
+    $qiitaPhpRecord = $title.",".$url.",".$date.",".$tag1.",".$tag2.",".$tag3.",".$tag4.",".$comments.",".$stock."\n";
+    file_put_contents($fileName, $qiitaPhpRecord, FILE_APPEND | LOCK_EX);
+  }
+}
