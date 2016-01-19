55d36449a7cd3ee85f527656bb1d9e6887b4c698
her0m31 <eda.1380@gmail.com>
2016-01-19 20:20:37 +0900

    リファクタリング

--- ml/class/Morpheme.php
+++ ml/class/Morpheme.php
@@ -1,157 +1,137 @@
 <?php
-/** サンプルプログラム : nlp_ai/training_code/class/Morpheme.php
-  * [説明]
-  * 形態素解析クラス。
-  * 日本語と英語の文書の形態素解析に対応する。
-  * 
-  * プログラムのエンコーディングはUTF-8。
+/**
+* 形態素解析クラス。
+* 日本語と英語の文書の形態素解析に対応する。
+*/
+
+class Morpheme {
+  // @var object php_mecabオブジェクト
+  var $mecab = null;
+
+  /**
+  * mecabを使って形態素解析する。
+  * @param  string text 形態素解析をする文章(UTF-8)
+  * @return array  形態素解析結果
   */
+  function parseJapanese($text) {
+    /** php_mecabオブジェクトが初期化されているかチェックする。
+    * もし初期化されていなければ、その場で初期化する。
+    */
+    if(!is_object($this->mecab)) {
+      $this->mecab = new MeCab_Tagger();
+    }
 
-class Morpheme
-{
-    /** 
-      * @var    object      php_mecabオブジェクト
-      */
-    var $mecab      = null;
-    
-    
-    /** mecabを使って形態素解析する。
-      * @param          string          text                形態素解析をする文章(UTF-8)
-      * @return         array           形態素解析結果
-      */
-    function parseJapanese( $text )
-    {
-        /** php_mecabオブジェクトが初期化されているかチェックする。
-          * もし初期化されていなければ、その場で初期化する。
-          */
-        if( !is_object( $this->mecab ) ){
-            $this->mecab    = new MeCab_Tagger();
-        }
-        
-        /* 形態素解析結果(戻り値用) */
-        $morphemeList   = array();
-        
-        /** 
-          * @var    array       mecabの形態素解析結果を1行ごとに分割したもの
-          */
-        $mecabResult    = explode( "\n" , $this->mecab->parse( $text ) );
-        
-        /* mecabの形態素解析結果を1行単位に分割したもの */
-        $resultSet      = explode( "\n" , $this->mecab->parse( $text ) );
-        
-        foreach( $resultSet as $eachResult )
-        {
-            if( substr( $eachResult , 0 , 3 ) !== 'EOS' ){
-                /* 形態素と情報部分に分割する */
-                list( $eachMorpheme , $eachInfo ) = explode( "\t" , $eachResult );
-                
-                /* 情報部分をリストに分割する */
-                $infoColumns        = explode( ',' , $eachInfo );
-                
-                /* よみがなが無い場合への対処*/
-                if( sizeof( $infoColumns ) < 8 ){
-                    $infoColumns[]  = '';
-                    $infoColumns[]  = '';
-                }
-                
-                /* 形態素解析結果(戻り値用)に追加する */
-                $morphemeList[]     = array( 'word'     => $eachMorpheme ,
-                                             'info'     => array( 'type'                => $infoColumns[0] ,
-                                                                  'category_1'          => $infoColumns[1] ,
-                                                                  'category_2'          => $infoColumns[2] ,
-                                                                  'category_3'          => $infoColumns[3] ,
-                                                                  'conjugation_type'    => $infoColumns[4] ,
-                                                                  'conjugation_form'    => $infoColumns[5] ,
-                                                                  'prototype'           => $infoColumns[6] ,
-                                                                  'yomigana'            => $infoColumns[7] ,
-                                                                  'yomigana_alt'        => $infoColumns[8] ) );
-            }else{
-                break;
-            }
-        }
-        return $morphemeList;
-    }
-    
-    
-    
-    /** 単語の前後に付着した余計な文字を掃除する。
-      * @param          string          word
-      * @return         string          掃除した後の単語
-      */
-    function cleanWord( $word )
-    {
-        while( true )
-        {
-            /* 掃除前の単語の文字列を記録 */
-            $lengthBefore   = strlen( $word );
-            $cleaned        = trim( $word , ' .\"\'\`\_' );
-            
-            /* trim()関数で掃除をしても変化がなければ、掃除終了と判定する */
-            if( strlen( $cleaned ) == $lengthBefore ){
-                break;
-            }else{
-                $word       = $cleaned;
-            }
-        }
-        return $word;
-    }
-    
-    
-    
-    /** 英語のテキストを単語に分割する。
-      * @param          string          text                分割する英語のテキスト
-      * @return         array           単語に分割した結果
-      */
-    function parseEnglish( $text )
-    {
-        /** 
-          * @var    string      セパレータ文字列
-          */
-        $separator  = " ,!?\"\|-+*/={}[]()<>:;@&^~\n\r\t";
-        
-        /**
-          * @var    array    形態素解析結果(戻り値用) 
-          */
-        $morphemeList   = array();
-        
-        /* 現在位置 */
-        $pos        = 0;
-        
-        /* 最大位置＝文字列数 */
-        $maxPos     = strlen( $text );
-        
-        /* バッファ文字列 */
-        $buffer     = '';
-        
-        while( $pos <= $maxPos )
-        {
-            $checkByte   = substr( $text , $pos , 1 );
-            if( strpos( $separator , $checkByte ) !== false ){
-                /* 区切り文字を検知した場合 */
-                if( strlen( $buffer ) > 0 ){
-                    $eachWord       = strtolower( $buffer );
-                    $eachWord       = $this->cleanWord( $eachWord );
-                    $morphemeList[] = array( 'word' => $eachWord );
-                    $buffer         = '';
-                }
-            }else{
-                if( strlen( $buffer ) > 0 ){
-                    $buffer     .= $checkByte;
-                }else{
-                    /* 単語候補の最初の文字を見つけた場合 */
-                    $buffer     = $checkByte;
-                }
-            }
-            ++$pos;
-        }
-        
-        if( strlen( $buffer ) > 0 ){
-            $eachWord       = strtolower( $buffer );
-            $eachWord       = $this->cleanWord( $eachWord );
-            $morphemeList[] = array( 'word' => $eachWord );
-        }
-        return $morphemeList;
-    }
-}
+    /* 形態素解析結果(戻り値用) */
+    $morphemeList = array();
 
+    /* @var array mecabの形態素解析結果を1行ごとに分割したもの */
+    $mecabResult = explode("\n", $this->mecab->parse($text));
+
+    /* mecabの形態素解析結果を1行単位に分割したもの */
+    $resultSet = explode("\n", $this->mecab->parse($text));
+
+    foreach($resultSet as $eachResult) {
+      if(substr($eachResult, 0, 3) !== 'EOS') {
+        /* 形態素と情報部分に分割する */
+        list($eachMorpheme, $eachInfo) = explode("\t", $eachResult);
+
+        /* 情報部分をリストに分割する */
+        $infoColumns = explode(',', $eachInfo);
+
+        /* よみがなが無い場合への対処*/
+        if(sizeof($infoColumns) < 8) {
+          $infoColumns[] = '';
+          $infoColumns[] = '';
+        }
+
+        /* 形態素解析結果(戻り値用)に追加する */
+        $morphemeList[] = array( 'word' => $eachMorpheme,
+          'info' => array('type'             => $infoColumns[0],
+                          'category_1'       => $infoColumns[1],
+                          'category_2'       => $infoColumns[2],
+                          'category_3'       => $infoColumns[3],
+                          'conjugation_type' => $infoColumns[4],
+                          'conjugation_form' => $infoColumns[5],
+                          'prototype'        => $infoColumns[6],
+                          'yomigana'         => $infoColumns[7],
+                          'yomigana_alt'     => $infoColumns[8]));
+      }
+    }
+
+    return $morphemeList;
+  }
+
+  /** 単語の前後に付着した余計な文字を掃除する。
+  * @param  string word
+  * @return string 掃除した後の単語
+  */
+  function cleanWord($word) {
+    while(true) {
+      /* 掃除前の単語の文字列を記録 */
+      $lengthBefore = strlen($word);
+      $cleaned      = trim($word, ' .\"\'\`\_');
+
+      /* trim()関数で掃除をしても変化がなければ、掃除終了と判定する */
+      if(strlen($cleaned) == $lengthBefore) {
+        break;
+      } else {
+        $word = $cleaned;
+      }
+    }
+
+    return $word;
+  }
+
+  /** 英語のテキストを単語に分割する。
+  * @param string text 分割する英語のテキスト
+  * @return array 単語に分割した結果
+  */
+  function parseEnglish($text) {
+    /** @var string セパレータ文字列 */
+    $separator = " ,!?\"\|-+*/={}[]()<>:;@&^~\n\r\t";
+      /* @var array 形態素解析結果(戻り値用) */
+      $morphemeList = array();
+
+      /* 現在位置 */
+      $pos = 0;
+
+      /* 最大位置＝文字列数 */
+      $maxPos = strlen($text);
+
+      /* バッファ文字列 */
+      $buffer = '';
+
+      while($pos <= $maxPos) {
+        $checkByte = substr($text, $pos, 1);
+
+        if(strpos($separator, $checkByte) !== false) {
+          /* 区切り文字を検知した場合 */
+          if(strlen($buffer) > 0) {
+            $eachWord       = strtolower($buffer);
+            $eachWord       = $this->cleanWord($eachWord);
+            $morphemeList[] = array('word' => $eachWord);
+            $buffer         = '';
+          }
+        } else {
+
+          if(strlen($buffer) > 0) {
+            $buffer .= $checkByte;
+          } else {
+            /* 単語候補の最初の文字を見つけた場合 */
+            $buffer = $checkByte;
+          }
+        }
+
+        ++$pos;
+      }
+
+      if(strlen($buffer) > 0) {
+        $eachWord       = strtolower($buffer);
+        $eachWord       = $this->cleanWord($eachWord);
+        $morphemeList[] = array('word' => $eachWord);
+      }
+
+      return $morphemeList;
+    }
+  }
 ?>
--- ml/code-5-3-3-j.php
+++ ml/code-5-3-3-j.php
@@ -1,78 +1,64 @@
 <?php
-/** サンプルプログラム : nlp_ai/training_code/code-5-3-3-j.php
-  * [説明]
-  * 「5.3.3.文書の判定用スコアを計算しよう：けんてーごっこコーパス編」の内容を扱うサンプルプログラム。
-  * 文書の判定用スコアを計算する。
-  * 
-  * プログラムのエンコーディングはUTF-8。
-  */
+/* 文書の判定用スコアを計算する */
 
 /* Corpus_Kentei クラスを読み込む */
-require_once( 'class/Corpus_Kentei.php' );
-
+require_once('class/Corpus_Kentei.php');
 /* Morpheme クラスを読み込む */
-require_once( 'class/Morpheme.php' );
-
+require_once('class/Morpheme.php');
 /* BayesLearning_Kentei クラスを読み込む */
-require_once( 'class/BayesLearning_Kentei.php' );
+require_once('class/BayesLearning_Kentei.php');
 
-/** 
-  * @var    object      BayesLearning_Kentei クラスから生成したオブジェクト
-  */
-$bayesKentei  = new BayesLearning_Kentei();
+/* @var object BayesLearning_Kentei クラスから生成したオブジェクト */
+$bayesKentei = new BayesLearning_Kentei();
 
 /* MySQLに接続するためのパスワードを記入すること */
 $bayesKentei->pdoPassword = '********';
 
-/** 
-  * @var    object      Corpus_Kentei クラスから生成したオブジェクト
-  */
-$ckentei      = new Corpus_Kentei();
+/* @var object Corpus_Kentei クラスから生成したオブジェクト */
+$ckentei = new Corpus_Kentei();
 
-if( is_object( $ckentei ) ){
-    if( is_object( $bayesKentei ) ){
-        printf( "***********************************************************\n" );
-        printf( "ベイジアンフィルタ : 文書の判定用スコアの計算\n" );
-        printf( "使用コーパス       : けんてーごっこ コーパス\n" );
-        printf( "***********************************************************\n" );
-        
-        /* 各カテゴリの分類辞書をメモリ上に読み込み、無作為に選択したコーパスの判定用スコアを計算する。 */
-        $categoryList       = $ckentei->getTrainingCategoryList();
-        
-        foreach( $categoryList as $eachCategory )
-        {
-            printf( "●カテゴリ : %s\n" , $eachCategory );
-            
-            /** 分類辞書をメモリ上に読み込む。
-              * 判定用スコアを計算するので、単語の最低出現回数は2回に引き下げる（第2引数に2を指定）
-              */
-            $bayesKentei->loadDictionary( $eachCategory , 2 );
-            
-            /* カテゴリに含まれるコーパスファイルの一覧を取得する */
-            $fileList       = $ckentei->getTrainingCorpusList( $eachCategory );
-            
-            shuffle( $fileList );
-            
-            /* 各カテゴリのコーパス10個を無作為に選択し、判定用スコアを計算し表示する */
-            foreach( $fileList as $eachPos => $eachFile )
-            {
-                if( $eachPos < 10 ){
-                    $corpusBody             = $ckentei->getTrainingContent( $eachFile );
-                    $documentScore          = $bayesKentei->culcDocumentScore( $corpusBody );
-                    printf( "コーパス %s : 判定用スコア = %f\n" , $eachFile , $documentScore );
-                }else{
-                    break;
-                }
-            }
-            printf( "\n\n" );
+if(is_object($ckentei)) {
+  if(is_object($bayesKentei)) {
+    printf("***********************************************************\n");
+    printf("ベイジアンフィルタ : 文書の判定用スコアの計算\n");
+    printf("使用コーパス       : けんてーごっこ コーパス\n");
+    printf("***********************************************************\n");
+
+    /* 各カテゴリの分類辞書をメモリ上に読み込み、無作為に選択したコーパスの判定用スコアを計算する */
+    $categoryList = $ckentei->getTrainingCategoryList();
+
+    foreach($categoryList as $eachCategory) {
+      printf("●カテゴリ : %s\n", $eachCategory);
+
+      /** 分類辞書をメモリ上に読み込む。
+      * 判定用スコアを計算するので、単語の最低出現回数は2回に引き下げる（第2引数に2を指定）
+      */
+      $bayesKentei->loadDictionary($eachCategory, 2);
+
+      /* カテゴリに含まれるコーパスファイルの一覧を取得する */
+      $fileList = $ckentei->getTrainingCorpusList($eachCategory);
+
+      shuffle($fileList);
+      
+      /* 各カテゴリのコーパス10個を無作為に選択し、判定用スコアを計算し表示する */
+      foreach($fileList as $eachPos => $eachFile) {
+        if($eachPos < 10) {
+          $corpusBody = $ckentei->getTrainingContent($eachFile);
+          $documentScore = $bayesKentei->culcDocumentScore($corpusBody);
+          printf("コーパス %s : 判定用スコア = %f\n", $eachFile, $documentScore);
+        } else {
+          break;
         }
-    }else{
-        printf( "BayesLearning_Kentei オブジェクトを作成できません。処理を中断します。\n" );
-        exit();
-    }
-}else{
-    printf( "Corpus_Kentei オブジェクトを作成できません。処理を中断します。\n" );
-    exit();
-}
+      }
 
+      printf("\n\n");
+    }
+  } else {
+    printf("BayesLearning_Kentei オブジェクトを作成できません。処理を中断します。\n");
+    exit();
+  }
+} else {
+  printf("Corpus_Kentei オブジェクトを作成できません。処理を中断します。\n");
+  exit();
+}
 ?>
