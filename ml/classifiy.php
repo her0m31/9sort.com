3457a7cdfaac0406ec4074db474c01197927721f
her0m31 <eda.1380@gmail.com>
2016-01-30 18:33:46 +0900

    引数のテキストの判定スコアを表示するスクリプトを追加

--- ml/classifiy.php
+++ ml/classifiy.php
@@ -13,34 +13,47 @@ $bayes->pdoPassword = 'root';
 $corpus = new Corpus();
 
 $classifiyScores = array();
+$corpusBody = null;
 
-if(is_object($corpus)) {
-  if(is_object($bayes)) {
-    printf("***********************************************************\n");
-    printf("ベイジアンフィルタ : 文書の判定用スコアの計算\n");
-    printf("***********************************************************\n");
-
-    /* 各カテゴリの分類辞書をメモリ上に読み込み、無作為に選択したコーパスの判定用スコアを計算する */
-    $categoryList = $corpus->getTrainingCategoryList();
-
-    foreach($categoryList as $eachCategory) {
-      printf("●カテゴリ : %s\n", $eachCategory);
-
-      /** 分類辞書をメモリ上に読み込む。
-      * 判定用スコアを計算するので、単語の最低出現回数は2回に引き下げる（第2引数に2を指定）
-      */
-      $bayes->loadDictionary($eachCategory, 2);
-
-      $classifiyScores[] = $bayes->culcDocumentScore($corpusBody);
-
-      printf("\n\n");
-    }
-  } else {
-    printf("BayesLearning オブジェクトを作成できません。処理を中断します。\n");
-    exit();
-  }
-} else {
-  printf("Corpus オブジェクトを作成できません。処理を中断します。\n");
+if(isset($argv[1]) === FALSE) {
+  printf("引数を指定してください\n");
   exit();
+}
+
+if(is_object($corpus) === FALSE) {
+  printf("Corpus オブジェクトを作成できません\n");
+  exit();
+}
+
+if(is_object($bayes) === FALSE) {
+  printf("BayesLearning オブジェクトを作成できません\n");
+  exit();
+}
+
+$corpusBody = $corpus->getContent($argv[1]);
+
+if(is_null($corpusBody)) {
+  exit();
+}
+
+printf("***********************************************************\n");
+printf("ベイジアンフィルタ : 文書の判定用スコアの計算\n");
+printf("***********************************************************\n");
+
+/* 各カテゴリの分類辞書をメモリ上に読み込み、無作為に選択したコーパスの判定用スコアを計算する */
+$categoryList = $corpus->getTrainingCategoryList();
+
+foreach($categoryList as $eachCategory) {
+  printf("カテゴリ : %s\n", $eachCategory);
+
+  /** 分類辞書をメモリ上に読み込む。
+  * 判定用スコアを計算するので、単語の最低出現回数は2回に引き下げる（第2引数に2を指定）
+  */
+  $bayes->loadDictionary($eachCategory, 2);
+
+  $classifiyScores = $bayes->culcDocumentScore($corpus->getContent($argv[1]));
+
+  printf("%s\n", $classifiyScores);
+  printf("\n\n");
 }
 ?>
